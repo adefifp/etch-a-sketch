c3ca3872abfd800830f391a6a91621c9d7e672fe
adefifp <edd1e20786@gmail.com>
2025-01-28 16:21:08 -0500

    added hovering and color transition, also moved all styling to css styles file

--- scripts.js
+++ scripts.js
@@ -1,19 +1,21 @@
 const body = document.querySelector('body')
-body.style.display = "flex"
 
 const gridContainer = document.getElementById("gridContainer")
 
 for(i = 0; i<16; i ++){
     const row = document.createElement('div')
     row.style.display = 'flex'
+
     for (j = 0; j <16; j++){
         const box = document.createElement('div');
         row.appendChild(box)
-        box.style.backgroundColor = "black"
-        box.style.width = '50px'
-        box.style.height = '50px'
-        box.style.margin = '10px'
+        box.classList.add("box")
+        box.addEventListener("mouseenter", () =>{
+            box.style.backgroundColor = "pink"
+        })
+        box.addEventListener("mouseleave" ,() => {
+            box.style.backgroundColor = "gray"
+        })
     }
     gridContainer.appendChild(row)
 }
-
