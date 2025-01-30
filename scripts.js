fc5a2ef4be8df8e2ea5e033460be12eaad182035
adefifp <edd1e20786@gmail.com>
2025-01-30 11:15:42 -0500

    added button functionality to generate new grid, have to make it conform to grid size

--- scripts.js
+++ scripts.js
@@ -6,24 +6,36 @@ const gridContainer = document.getElementById("gridContainer")
 body.appendChild(button);
 body.appendChild(gridContainer)
 
-
-for(i = 0; i<16; i ++){
-    const row = document.createElement('div')
-    row.style.display = 'flex'
-    row.style.width = '100%'
-
-    for (j = 0; j <16; j++){
-        const box = document.createElement('div');
-        row.appendChild(box)
-        box.classList.add("box")
-        box.addEventListener("mouseenter", () =>{
-            box.style.backgroundColor = "pink"
-        })
-        box.addEventListener("mouseleave" ,() => {
-            box.style.backgroundColor = "gray"
-        })
+button.addEventListener('click', () =>{
+    let userInput = prompt("Please enter the dimension of the square grid you'd like.")
+    if(userInput === 'null' || userInput <= 0 || userInput ===""){
+        alert('Please enter a valid input')
+        return;
     }
-    gridContainer.appendChild(row)
-}
+    if(userInput > 100){
+        alert('Maximum is 100.')
+        return;
+    }
+    gridContainer.innerHTML = "";
+    for(i = 0; i<userInput; i ++){
+        const row = document.createElement('div')
+        row.style.display = 'flex'
+        row.style.width = '100%'
+    
+        for (j = 0; j <userInput; j++){
+            const box = document.createElement('div');
+            row.appendChild(box)
+            box.classList.add("box")
+            box.addEventListener("mouseenter", () =>{
+                box.style.backgroundColor = "pink"
+            })
+            box.addEventListener("mouseleave" ,() => {
+                box.style.backgroundColor = "gray"
+            })
+        }
+        gridContainer.appendChild(row)
+    }
+    
+})
 
 
