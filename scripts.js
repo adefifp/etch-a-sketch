e13c2870ed3ede53780e0298381b64b70c49a5b1
adefifp <edd1e20786@gmail.com>
2025-01-30 11:06:45 -0500

    added button

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
 const body = document.querySelector('body')
+const button = document.querySelector('button')
+
 
 const gridContainer = document.getElementById("gridContainer")
+body.appendChild(button);
+body.appendChild(gridContainer)
+
 
 for(i = 0; i<16; i ++){
     const row = document.createElement('div')
     row.style.display = 'flex'
+    row.style.width = '100%'
 
     for (j = 0; j <16; j++){
         const box = document.createElement('div');
@@ -19,3 +25,5 @@ for(i = 0; i<16; i ++){
     }
     gridContainer.appendChild(row)
 }
+
+
